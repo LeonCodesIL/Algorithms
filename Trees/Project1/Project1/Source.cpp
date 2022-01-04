e7d2c3599cc12c2014745682333007ad54f895f1
LeonCodesIL <LeonKukkerPro@gmail.com>
2022-01-04 23:52:10 +0200

    progress in c++ binary tree with pointers

--- Trees/Project1/Project1/Source.cpp
+++ Trees/Project1/Project1/Source.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
 
 using namespace std;
-int main()
-{
-	BTnode r("first", NULL, NULL, NULL);
-	printf("%s", r.value);
-}
-
 
 class BTnode {
   public:
@@ -15,10 +11,26 @@ class BTnode {
 	BTnode *right;
 	BTnode *parent;
 
-	BTnode(string val, BTnode* leftSide, BTnode* rightSide, BTnode* newParent) {
+	BTnode(string val, BTnode *leftSide, BTnode* rightSide, BTnode* newParent) {
 		 value = val;
 		 left = leftSide;
 		 right = rightSide;
 		 parent = newParent;
 	}
 };
+//code not working, figure out if its possible to assaign one object to another like in javascript
+int main() {
+	BTnode r("first", NULL, NULL, NULL);
+	BTnode m("1", NULL, NULL, &r);
+	r.left = &m;
+	std::cout << &r << endl;
+	std::cout << m.parent << endl;
+	int depth = 0;
+	
+	while (&m != &r) {
+		m = *m.parent;
+		depth++;
+	}
+	std::cout << &r << endl;
+	std::cout << &m << endl;
+}
