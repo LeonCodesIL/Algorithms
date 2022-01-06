dd9022780af8abc423d7571cdb7c25a773c522b9
LeonCodesIL <LeonKukkerPro@gmail.com>
2022-01-06 14:51:39 +0200

    progress in c++ tree implementation

--- Trees/Project1/Project1/Source.cpp
+++ Trees/Project1/Project1/Source.cpp
@@ -11,26 +11,41 @@ class BTnode {
 	BTnode *right;
 	BTnode *parent;
 
-	BTnode(string val, BTnode *leftSide, BTnode* rightSide, BTnode* newParent) {
+	BTnode(string val, BTnode *leftSide, BTnode *rightSide, BTnode *newParent) {
 		 value = val;
 		 left = leftSide;
 		 right = rightSide;
 		 parent = newParent;
 	}
 };
-//code not working, figure out if its possible to assaign one object to another like in javascript
-int main() {
-	BTnode r("first", NULL, NULL, NULL);
-	BTnode m("1", NULL, NULL, &r);
-	r.left = &m;
-	std::cout << &r << endl;
-	std::cout << m.parent << endl;
+
+int depth(BTnode node, BTnode root) {
 	int depth = 0;
-	
-	while (&m != &r) {
-		m = *m.parent;
+	while (node.value != root.value) {
+		node = *node.parent;
 		depth++;
 	}
+	return depth;
+}
+
+int size(BTnode *node) {
+	if (!node) { return 0; }
+	return 1 + size(node->left) + size(node->right);
+}
+
+int main() {
+	BTnode r("1", NULL, NULL, NULL);
+	BTnode m("2", NULL, NULL, &r);
+	BTnode k("3", NULL, NULL, &m);
+
+	r.left = &m;
+	m.right = &k;
+
+	m = *m.parent;
+	
 	std::cout << &r << endl;
-	std::cout << &m << endl;
+	std::cout << &m.parent << endl;
+
+	std::cout << r.value << endl;
+	std::cout << m.value << endl;
 }
